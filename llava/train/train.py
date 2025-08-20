bbaaee09c06a8ddbf97be06a6dbcf8418cccf63b
HayatoHongo <151999571+HayatoHongo@users.noreply.github.com>
2025-08-20 21:34:12 +0900

    added mm_patch_merge_type to class ModelArguments

--- llava/train/train.py
+++ llava/train/train.py
@@ -57,6 +57,7 @@ class ModelArguments:
     mm_projector_type: Optional[str] = field(default='linear')
     mm_use_im_start_end: bool = field(default=False)
     mm_use_im_patch_token: bool = field(default=True)
+    mm_patch_merge_type: Optional[str] = field(default='flat')
     mm_vision_select_feature: Optional[str] = field(default="patch")
 
 
