35cd447da1947c7da09b4f583f2d96f13f1af2e3
HayatoHongo <151999571+HayatoHongo@users.noreply.github.com>
2025-10-05 09:08:07 +0000

    updated train batch size from 1 to 2

--- scripts/v1_5/pretrain.sh
+++ scripts/v1_5/pretrain.sh
@@ -15,8 +15,8 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir ./checkpoints/Tinyllava-v1.5-7b-pretrain \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
